ab60d88dbac149243ec03b5604041119adae8467
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-21 17:47:15 +0300

    home page

--- online-school/src/App.jsx
+++ online-school/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CourseList from './components/CourseList';
---
+++ online-school/src/components/CourseList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CourseList() {
+  return (
+    <div>
+      <h1>Список курсов</h1>
+      <p>Здесь будет список доступных курсов с возможностью фильтрации.</p>
+    </div>
+  );
+}
+
+export default CourseList; // Экспорт по умолчанию
