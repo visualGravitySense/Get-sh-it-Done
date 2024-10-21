0a3c77fee1fb9c3dd325a2888e27026df4699053
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-21 17:29:13 +0300

    home

---
+++ online-school/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>О нашей школе</h1>
+      <p>Мы предлагаем лучшие онлайн-курсы по различным дисциплинам.</p>
+    </div>
+  );
+}
+
+export default About; // Экспорт компонента по умолчанию
---
+++ online-school/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Контакты</h1>
+      <p>Свяжитесь с нами для получения дополнительной информации.</p>
+    </div>
+  );
+}
+
+export default Contact;
